f0112f2ecf5e65b02ada19570755095b7e4de02e
Marcelo Soares <soares.srp@gmail.com>
2021-05-20 15:49:32 -0300

    25. Tornando o mapa mais dinâmico usandos testes com TDD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,19 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  return (
+    <Map
+      places={[
+        {
+          id: '1',
+          name: 'Santos',
+          slug: 'santos',
+          location: {
+            latitude: -23.9618,
+            longitude: -46.3322
+          }
+        }
+      ]}
+    />
+  )
 }
