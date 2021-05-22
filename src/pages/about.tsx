7e892a0dc799edb3b98c34375d942ccd95deb1ee
Marcelo Soares <soares.srp@gmail.com>
2021-05-22 15:09:59 -0300

    30. Configurando o GraphQL client

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,17 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const About = () => {
+export default function AboutPage() {
   return <AboutTemplate />
 }
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
