875f786f842faba9134f743310ba74567631fbb3
Marcelo Soares <soares.srp@gmail.com>
2021-05-24 17:45:55 -0300

    37. Criando rotas dinâmicas para as páginas de Places

---
+++ src/pages/place/[slug].tsx
@@ -0,0 +1,47 @@
+import client from 'graphql/client'
+import { GetPlacesQuery, GetPlaceBySlugQuery } from 'graphql/generated/graphql'
+import { GET_PLACES, GET_PLACE_BY_SLUG } from 'graphql/queries'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/dist/client/router'
+import PlacesTemplate, { PlacesTemplateProps } from 'templates/Places'
+
+export default function Place({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+
+  // retorna um loading ou qualquer cois enquanto tá sendo criado
+  if (router.isFallback) return null
+
+  return <PlacesTemplate place={place} />
+}
+
+export async function getStaticPaths() {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, {
+    first: 3
+  })
+
+  const paths = places.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { place } = await client.request<GetPlaceBySlugQuery>(
+    GET_PLACE_BY_SLUG,
+    {
+      slug: `${params?.slug}`
+    }
+  )
+
+  if (!place) return { notFound: true }
+
+  return {
+    props: {
+      place
+    }
+  }
+}
